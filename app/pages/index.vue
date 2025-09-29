e4554d4823225088278a56a36f16708347f1b78c
Jimwell Rabino <jimwell.rabino@thynker.tech>
2025-09-29 20:34:50 +0800

    Coming Soon Page

---
+++ app/pages/index.vue
@@ -0,0 +1,100 @@
+<template>
+<section class="relative min-h-screen">
+    <div class="absolute -z-10 w-full h-full top-0 left-0">
+        <svg class="w-full h-full absolute" width="1728" height="1117" viewBox="0 0 1728 1117" fill="none" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice">
+        <g clip-path="url(#clip0_213_7)">
+            <rect width="1728" height="1117" class="fill-primary-0" />
+            <g filter="url(#filter0_f_213_7)">
+            <circle cx="864.5" cy="1143.5" r="420.5" class="fill-primary-1000/60" />
+            </g>
+            <g filter="url(#filter1_f_213_7)">
+            <rect x="242" y="-103.26" width="132.291" height="651" transform="rotate(-25.8781 242 -103.26)" class="fill-primary-1000" />
+            </g>
+            <g filter="url(#filter2_f_213_7)">
+            <rect x="1786" y="1012" width="132.291" height="651" transform="rotate(90 1786 1012)" class="fill-primary-1000" />
+            </g>
+        </g>
+        <defs>
+            <filter id="filter0_f_213_7" x="44" y="323" width="1641" height="1641" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+            <feFlood flood-opacity="0" result="BackgroundImageFix" />
+            <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
+            <feGaussianBlur stdDeviation="200" result="effect1_foregroundBlur_213_7" />
+            </filter>
+            <filter id="filter1_f_213_7" x="-58" y="-461" width="1003.16" height="1243.46" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+            <feFlood flood-opacity="0" result="BackgroundImageFix" />
+            <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
+            <feGaussianBlur stdDeviation="150" result="effect1_foregroundBlur_213_7" />
+            </filter>
+            <filter id="filter2_f_213_7" x="895" y="772" width="1131" height="612.291" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+            <feFlood flood-opacity="0" result="BackgroundImageFix" />
+            <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
+            <feGaussianBlur stdDeviation="120" result="effect1_foregroundBlur_213_7" />
+            </filter>
+            <clipPath id="clip0_213_7">
+            <rect width="1728" height="1117" class="fill-primary-0" />
+            </clipPath>
+        </defs>
+        </svg>
+    </div>
+
+    <div class="mx-auto max-w-7xl px-6 pt-18 lg:pt-20 pb-12 lg:px-8 py-20 flex flex-col">
+        <div class="mx-auto flex max-w-7xl flex-col items-center">
+        <div class="z-10 mx-auto flex max-w-7xl flex-col items-center gap-6 text-center">
+            <div class="flex flex-col items-center">
+                <div class="flex text-[10px] lg:text-sm text-gray-200 flex-row w-fit items-center px-6 rounded-full mx-10  gap-1 mb-7 py-1.5 bg-gradient-to-r from-hu-violet via-[#972e92] to-hu-blue" data-aos="fade-up" data-aos-delay="200">
+                    <span class="font-body font-medium">Are you ready?</span>
+                </div>
+                <div class="w-15 lg:w-18 my-2">
+                    <img src="/img/hikeup_logo_single.png" alt="Hike Up">
+                </div>
+                <h1 class="mt-14 md:my-10 mb-6 font-display font-medium text-secondary-1200 tracking-tight text-5xl sm:text-7xl lg:text-[90px] text-balance text-center"><span class="text-hu-violet">Hike Up Marketing</span> is Coming Soon!</h1>
+                <p class="font-body font-normal text-gray-600 text-md md:text-xl text-balance mx-auto">Our website is launching soon. We help businesses grow with digital marketing and IT solutions.</p>
+            </div>
+
+            <div class="mt-6 grid grid-cols-1 gap-y-2 gap-4 w-full md:w-80">
+                <div class="relative font-body">
+                    <input type="text" id="floating_filled" class="block rounded-t-lg px-2.5 pb-2.5 pt-5 w-full text-sm text-white bg-primary-800/50 border-0 border-b-2 border-primary-800 appearance-none focus:outline-none focus:ring-0 focus:border-primary-1000 peer" placeholder=" " />
+                    <label for="floating_filled" class="absolute text-sm text-primary-1200 duration-300 transform -translate-y-4 scale-75 top-4 z-10 origin-[0] start-2.5 peer-focus:text-primary-900 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto">Your Email</label>
+                </div>
+                <button href="#" class="flex font-display items-center cursor-pointer bg-primary-1400 text-white border-3 border-primary-1300 gap-x-3 px-3 md:px-4 py-2 min-h-14 text-base md:text-md rounded-lg">
+                    <div class="flex items-center flex-1 text-center justify-center whitespace-nowrap px-2 md:px-3">Join Newsletter</div>
+                </button>
+            </div>
+
+            <div class="mt-10 md:mt-10 lg:mt-8 flex flex-col items-center gap-4">
+                <p class="font-display text-primary-1500 font-medium text-lg text-center">Visit Our Socials</p>
+                <div class="flex flex-wrap items-center justify-center gap-5 opacity-90">
+                    <!-- Facebook -->
+                    <NuxtLink
+                    to="https://facebook.com/hikeupmarketing"
+                    target="_blank"
+                    class="text-primary-1200 hover:text-blue-500 transition-colors duration-300"
+                    >
+                    <i class="fa-brands fa-facebook text-3xl"></i>
+                    </NuxtLink>
+
+                    <!-- Instagram -->
+                    <NuxtLink
+                    to="https://instagram.com/hikeupmarketing"
+                    target="_blank"
+                    class="text-primary-1200 hover:text-violet-500 transition-colors duration-300"
+                    >
+                    <i class="fa-brands fa-instagram text-3xl"></i>
+                    </NuxtLink>
+
+                    <!-- LinkedIn -->
+                    <NuxtLink
+                    to="https://linkedin.com/in/hikeupmarketing"
+                    target="_blank"
+                    class="text-primary-1200 hover:text-blue-500 transition-colors duration-300"
+                    >
+                    <i class="fa-brands fa-linkedin text-3xl"></i>
+                    </NuxtLink>
+                </div>
+            </div>
+        </div>
+        </div>
+    </div>
+</section>
+
+</template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,47 @@
+import tailwindcss from "@tailwindcss/vite";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      title: 'Hike Up Marketing - Coming Soon',
+      htmlAttrs: {
+        lang: 'en',
+      },
+      link: [
+        { rel: 'apple-touch-icon', sizes: '57x57', href: '/favicon/apple-icon-57x57.png' },
+        { rel: 'apple-touch-icon', sizes: '60x60', href: '/favicon/apple-icon-60x60.png' },
+        { rel: 'apple-touch-icon', sizes: '72x72', href: '/favicon/apple-icon-72x72.png' },
+        { rel: 'apple-touch-icon', sizes: '76x76', href: '/favicon/apple-icon-76x76.png' },
+        { rel: 'apple-touch-icon', sizes: '114x114', href: '/favicon/apple-icon-114x114.png' },
+        { rel: 'apple-touch-icon', sizes: '120x120', href: '/favicon/apple-icon-120x120.png' },
+        { rel: 'apple-touch-icon', sizes: '144x144', href: '/favicon/apple-icon-144x144.png' },
+        { rel: 'apple-touch-icon', sizes: '152x152', href: '/favicon/apple-icon-152x152.png' },
+        { rel: 'apple-touch-icon', sizes: '180x180', href: '/favicon/apple-icon-180x180.png' },
+        { rel: 'icon', type: 'image/png', sizes: '192x192', href: '/favicon/android-icon-192x192.png' },
+        { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon/favicon-32x32.png' },
+        { rel: 'icon', type: 'image/png', sizes: '96x96', href: '/favicon/favicon-96x96.png' },
+        { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon/favicon-16x16.png' },
+        {
+          rel: "stylesheet",
+          href: "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css",
+          integrity: "sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw==",
+          crossorigin: "anonymous",
+          referrerpolicy: "no-referrer"
+        }
+      ],
+      charset: 'utf-16',
+      viewport: 'width=device-width, initial-scale=1, maximum-scale=1',
+    }
+  },
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+  css: [
+    '~/assets/main.css',
+  ],
 })
