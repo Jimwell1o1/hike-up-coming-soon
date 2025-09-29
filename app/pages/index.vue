b163f27bef0ba9faaa8fdcbcc6505a221bdd7015
Jimwell Rabino <jimwell.rabino@thynker.tech>
2025-09-29 20:40:06 +0800

    CSS adjustments

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -48,10 +48,10 @@
                     <img src="/img/hikeup_logo_single.png" alt="Hike Up">
                 </div>
                 <h1 class="mt-14 md:my-10 mb-6 font-display font-medium text-secondary-1200 tracking-tight text-5xl sm:text-7xl lg:text-[90px] text-balance text-center"><span class="text-hu-violet">Hike Up Marketing</span> is Coming Soon!</h1>
-                <p class="font-body font-normal text-gray-600 text-md md:text-xl text-balance mx-auto">Our website is launching soon. We help businesses grow with digital marketing and IT solutions.</p>
+                <p class="mt-8 md:mt-4 font-body font-normal text-gray-600 text-lg md:text-xl text-balance mx-auto">Our website is launching soon. We help businesses grow with digital marketing and IT solutions.</p>
             </div>
 
-            <div class="mt-6 grid grid-cols-1 gap-y-2 gap-4 w-full md:w-80">
+            <!-- <div class="mt-6 grid grid-cols-1 gap-y-2 gap-4 w-full md:w-80">
                 <div class="relative font-body">
                     <input type="text" id="floating_filled" class="block rounded-t-lg px-2.5 pb-2.5 pt-5 w-full text-sm text-white bg-primary-800/50 border-0 border-b-2 border-primary-800 appearance-none focus:outline-none focus:ring-0 focus:border-primary-1000 peer" placeholder=" " />
                     <label for="floating_filled" class="absolute text-sm text-primary-1200 duration-300 transform -translate-y-4 scale-75 top-4 z-10 origin-[0] start-2.5 peer-focus:text-primary-900 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto">Your Email</label>
@@ -59,9 +59,9 @@
                 <button href="#" class="flex font-display items-center cursor-pointer bg-primary-1400 text-white border-3 border-primary-1300 gap-x-3 px-3 md:px-4 py-2 min-h-14 text-base md:text-md rounded-lg">
                     <div class="flex items-center flex-1 text-center justify-center whitespace-nowrap px-2 md:px-3">Join Newsletter</div>
                 </button>
-            </div>
+            </div> -->
 
-            <div class="mt-10 md:mt-10 lg:mt-8 flex flex-col items-center gap-4">
+            <div class="mt-30 md:mt-10 lg:mt-8 flex flex-col items-center gap-4">
                 <p class="font-display text-primary-1500 font-medium text-lg text-center">Visit Our Socials</p>
                 <div class="flex flex-wrap items-center justify-center gap-5 opacity-90">
                     <!-- Facebook -->
